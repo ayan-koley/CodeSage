c1aa20cae85bb49533a75af85eb2cc9d738da8a5
Ayan Koley <koleyayan07@gmail.com>
2026-04-21 11:34:38 +0530

    Add linting service using ESLint for code analysis

---
+++ backend/src/services/lint.services.js
@@ -0,0 +1,37 @@
+import { ESLint } from 'eslint';
+
+const mapLintToIssue = (msg) => {
+    return {
+        type: msg.severity === 2 ? "bug" : "warning",
+        ruleId: msg.ruleId,
+        title: msg.ruleId || "Unknown Issue",
+        severity: getSeverity(msg),
+        explanation: msg.message,
+        fix: msg.suggestions?.[0]?.desc || null,
+        lineNumber: msg.line,
+    };
+}
+
+const getSeverity = (msg) => {
+  if (msg.severity === 2) return "high";    
+
+  if (msg.ruleId === "no-unused-vars") return "low";
+
+  return "medium";
+};
+
+export const runLint = async (code, fileName="file.js") => {
+    try {
+        // 1. create an instance
+        const eslint = new ESLint({
+            overrideConfigFile: "eslint.config.js"
+        });
+        // lint text
+        const results = await eslint.lintText(code, { filePath: fileName });
+        const lintResult = results[0].messages;
+        
+        return lintResult.map((msg) => mapLintToIssue(msg));
+    } catch (e) {
+        console.error('Error occurred while running ESLint:', e.message);
+    }
+};
