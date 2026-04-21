f98f55a2974bc739793b179165885d86c07e3447
Ayan Koley <koleyayan07@gmail.com>
2026-04-21 15:14:07 +0530

    Add index.js to export all rule modules

---
+++ backend/src/rules/index.js
@@ -0,0 +1,20 @@
+import { maxParamsRule } from "./maxParamsRule";
+import { noAnonymousFunctionRule } from "./noAnonymousFunctionRule";
+import { noConsoleLogRule } from "./noConsoleLogRule";
+import { noDebuggerRule } from "./noDebuggerRule";
+import { noEmptyFunctionRule } from './noEmptyFunctionRule.js'
+import { noVarRule } from "./novarRule.js";
+import { strictEqualityRule } from './strictEqualityRule.js'
+import { unusedVariableRule } from "./unusedVariableRule.js";
+
+
+export {
+    maxParamsRule,
+    noAnonymousFunctionRule,
+    noConsoleLogRule,
+    noDebuggerRule,
+    noEmptyFunctionRule,
+    noVarRule,
+    strictEqualityRule,
+    unusedVariableRule
+}
