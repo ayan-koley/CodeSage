11960a776a03a56e570e6212b60cfa11757eeb59
Ayan Koley <koleyayan07@gmail.com>
2026-04-23 23:17:23 +0530

    feat: add ApiResponse class for standardized API response handling

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+export class ApiResponse {
+    constructor(statusCode, data = null, message) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode >= 200 && statusCode < 300;
+    }
+}
