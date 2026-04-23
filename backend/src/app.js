da333b12c3f9b6ad6d1bfd6fe3bc2312c056c6cd
Ayan Koley <koleyayan07@gmail.com>
2026-04-23 23:18:01 +0530

    feat: set up express app with JSON parsing, URL encoding, and CORS support

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,19 @@
 import exprss from 'express';
+import cors from 'cors';
 
 const app = exprss();
+app.use(exprss.json());
+app.use(exprss.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+import analysisRoute from "./routes/analysis.routes.js";
+app.use("/api/v1/analysis", analysisRoute);
+app.use("/api/v1/healthcheck", (req, res) => {
+    return res.status(200).json({message: "Server is healthy"});
+})
 
 export default app;
