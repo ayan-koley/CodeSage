7bbc367576542c18c1ca5615b3ede1523e62d1b6
Ayan Koley <koleyayan07@gmail.com>
2026-04-23 23:17:55 +0530

    refactor: comment out database connection logic in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
+import "dotenv/config";
 import app from "./app.js";
 import { connectDB } from "./db/index.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+// }).catch((err) => {
+//     console.error("Error connecting to the database:", err);
+//     process.exit(1);
+// });
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-}).catch((err) => {
-    console.error("Error connecting to the database:", err);
-    process.exit(1);
-});
+})
