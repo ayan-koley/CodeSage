796c93bcc187af214c70f6ad2900a2f04497ca03
Ayan Koley <koleyayan07@gmail.com>
2026-05-05 18:00:38 +0530

    feat: update key generation for issue mapping and enhance conflict resolution logic

--- backend/src/pipeline/resolveConflicts.js
+++ backend/src/pipeline/resolveConflicts.js
@@ -3,22 +3,19 @@ export const resolveConflicts = (eslintIssues, astIssues) => {
 
     // add eslint issues to the map
     eslintIssues.forEach(issue => {
-        const key = `${issue.lineNumber}-${issue.column}`;
+        const key = `${issue.ruleId}-${issue.lineNumber}-${issue.name}`;
         map.set(key, issue);
     });
 
     // add ast issues and if conflict accurs, add both 
     astIssues.forEach(issue => {
-        const key = `${issue.lineNumber}-${issue.column}`;
+        const key = `${issue.ruleId}-${issue.lineNumber}-${issue.name}`;
 
         if(map.has(key)) {
             const existing = map.get(key);
 
-            existing.suggestions.push(...existing.suggestions, ...issue.suggestions);
-            existing.source.push(new Set([
-                ...existing.source,
-                ...issue.source
-            ]));
+            existing.message.push(...issue.message);
+            existing.source.push(...issue.source);
             existing.type = "architecture";
         }   else {
             map.set(key, issue);
