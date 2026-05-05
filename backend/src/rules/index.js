1c671a25464f537b56cab6e370974324195ff158
Ayan Koley <koleyayan07@gmail.com>
2026-05-05 17:59:56 +0530

    feat: simplify rule exports by removing unused linting rules

--- backend/src/rules/index.js
+++ backend/src/rules/index.js
@@ -1,22 +1,5 @@
-// TODO: delete all rules and make architectural rules here not repeated rules
-
-import { maxParamsRule } from "./maxParamsRule.js";
-import { noAnonymousFunctionRule } from "./noAnonymousFunctionRule.js";
-import { noConsoleLogRule } from "./noConsoleLogRule.js";
-import { noDebuggerRule } from "./noDebuggerRule.js";
-import { noEmptyFunctionRule } from './noEmptyFunctionRule.js'
-import { noVarRule } from "./novarRule.js";
-import { strictEqualityRule } from './strictEqualityRule.js'
-import { unusedVariableRule } from "./unusedVariableRule.js";
-
+import { noUnusedFunctionRule } from "./noUsedFunction.js";
 
 export {
-    maxParamsRule,
-    noAnonymousFunctionRule,
-    noConsoleLogRule,
-    noDebuggerRule,
-    noEmptyFunctionRule,
-    noVarRule,
-    strictEqualityRule,
-    unusedVariableRule
+    noUnusedFunctionRule
 }
