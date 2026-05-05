4acc6760a5553d0fb9dbc94169fe00fbbca40251
Ayan Koley <koleyayan07@gmail.com>
2026-05-05 17:59:47 +0530

    feat: remove deprecated linting rules for better code quality

--- backend/src/rules/noAnonymousFunctionRule.js
+++
@@ -1,9 +0,0 @@
-export function noAnonymousFunctionRule(path) {
-     if(path.isFunctionDeclaration() && !path.node.id?.name) {
-        return {
-            type: "info",
-            message: "Anonymous function detected",
-            line: path.node.loc.start.line,
-        }
-     }
-}
--- backend/src/rules/noDebuggerRule.js
+++
@@ -1,9 +0,0 @@
-export function noDebuggerRule(path) {
-  if (path.isDebuggerStatement()) {
-    return {
-      type: "error",
-      message: "Remove debugger statement",
-      line: path.node.loc.start.line,
-    };
-  }
-}
