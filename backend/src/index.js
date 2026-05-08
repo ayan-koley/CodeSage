887dc0991dcc626a609a3e26d1e539586f3cc6ce
Ayan Koley <koleyayan07@gmail.com>
2026-05-08 18:58:27 +0530

    fix: correct indentation and formatting for database connection logic in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,14 @@ import { connectDB } from "./db/index.js";
 
 const PORT = process.env.PORT || 5000;
 
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-// }).catch((err) => {
-//     console.error("Error connecting to the database:", err);
-//     process.exit(1);
-// });
+connectDB().then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+}).catch((err) => {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
